7c96f68a352552d2d7f9ce2ed8900747a92e3c6e
Cyrus <valkaniotist@gmail.com>
2018-09-18 13:18:19 +0300

    started implementing views to test the connections to the dbs and the login/signup logic

--- src/views/testauth.go
+++ src/views/testauth.go
@@ -1,9 +1,13 @@
 package views
 
 import (
-	"fmt"
+	"datastorage"
+	"html/template"
+	"log"
 	"middleware"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func init() {
@@ -23,9 +27,70 @@ will be used to test the gatekeeper
 and the role middleware
 */
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world")
+	t := template.Must(template.ParseFiles("./templates/index.html"))
+	t.Execute(w, nil)
+}
+
+/*
+login function
+passes the credentials provided to the gatekeeper for validation
+must implement a login function in the gatekeeper
+*/
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		postlogin(w, r)
+	} else {
+		t := template.Must(template.ParseFiles("./templates/login.html"))
+		t.Execute(w, nil)
+	}
+}
+
+/*
+signup function
+registers test users
+*/
+func signup(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		postsignup(w, r)
+	} else {
+		t := template.Must(template.ParseFiles("./templates/signup.html"))
+		t.Execute(w, nil)
+	}
+}
+
+/*
+postsignup function
+if user gets succesfully registered
+log him in and redirect. to be implemented
+*/
+func postsignup(w http.ResponseWriter, r *http.Request) {
+	username := r.PostFormValue("username")
+	password := r.PostFormValue("password")
+	dbclient, _ := datastorage.GetDataRouter().GetDb("common")
+	db := dbclient.GetMysqlClient()
+	stmt, _ := db.Prepare("INSERT INTO accounts VALUES(?,?);")
+	hash, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	_, err := stmt.Exec(username, hash)
+	if err != nil {
+		log.Println("post signup error", err)
+		return
+	}
+}
+
+/*
+postlogin function
+asks gatekeeper if the credentials provided are ok
+if they are redirects to secret
+if not must return message to session and redirect to login page
+either way a session token must be generated
+and then check whether the token is authenticated or not
+*/
+func postlogin(w http.ResponseWriter, r *http.Request) {
+	//offload the login function to the gatekeeper
 }
 
 func secret(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Secret view ok")
+	t := template.New("index")
+	t, _ = t.ParseFiles("./templates/secret.html")
+	t.Execute(w, nil)
 }
