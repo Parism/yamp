9b5ebbd594cb688ebbb75318c67dc5321082b3ea
cyrus <valkaniotist@gmail.com>
2018-10-25 19:11:30 +0300

    added more roles

--- src/variables/variables.go
+++ src/variables/variables.go
@@ -1,10 +1,25 @@
 package variables
 
 //ADMIN var used for admin role
-var ADMIN = 3
+var ADMIN = 8
+
+//DITIS var used for ditis role
+var DITIS = 7
+
+//YDITIS var used for ditis role
+var YDITIS = 6
+
+//P1G var used for P1G role
+var P1G = 5
 
 //CAPTAIN var used for captain role
-var CAPTAIN = 2
+var CAPTAIN = 4
+
+//VICE_CAPTAIN var used for vice captain role
+var VICE_CAPTAIN = 3
+
+//GMT var used for gmt role
+var GMT = 2
 
 //USER var used for user role
 var USER = 1
